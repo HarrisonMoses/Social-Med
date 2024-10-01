dc64a470ecf58795aa29577d9d8d4599cf083c16
Mugoya Moses <harrisonmoses64@gmail.com@student.mak.ac.ug>
2024-10-01 15:23:02 +0300

    Modified:Team Component

---
+++ src/Data/TeamsData.js
@@ -0,0 +1,35 @@
+ const teamdata = [
+   {
+     name: "Mugoya Moses",
+     Role: "Software Engineer",
+     image:
+       "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png",
+     description:
+       "Moses specializes in full-stack development and has extensive experience in creating responsive web applications.",
+   },
+   {
+     name: "Muhangi Elioda",
+     Role: "Software Engineer",
+     image:
+     "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png",
+     description:
+     "Muhangi is known for his proficiency in back-end technologies and ensuring robust server-side logic.",
+    },
+    {
+      name: "Heli Prajapati",
+      Role: "Software Engineer",
+      image:
+        "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png",
+      description:
+        "Heli has a strong foundation in web development and can help you build a successful and user-friendly website.",
+    },
+   {
+     name: "Mpagi Nelly",
+     Role: "Software Engineer",
+     image:
+       "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png",
+     description:
+       "Mpagi excels in user experience design and front-end development, making sure websites are both beautiful and functional.",
+   },
+ ];
+export default teamdata;
