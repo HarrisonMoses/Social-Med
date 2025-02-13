6c827c000750db7172a2a9f811de6d555f848863
Moses Mugoya <mugoya.moses@students.mak.ac.ug>
2025-02-13 09:28:53 +0300

    Modified config file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.JPG"],
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true,
+  },
 });
