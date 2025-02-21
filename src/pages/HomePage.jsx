ea05982efa3f8066c817fe8ec63c123197a8551a
Moses Mugoya <mugoya.moses@students.mak.ac.ug>
2025-02-21 10:15:10 +0300

    Fix: Back-ground

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
 
       <Nav />
       </div>
-      <div className="bg-[url('./soil.jpeg')] bg-cover h-screen rounded-b-sm over-flow relative"> 
+      <div className="bg_image bg-cover h-screen rounded-b-sm over-flow relative"> 
         <div className="backdrop-blur-xl bg-black bg-opacity-10 h-full flex flex-col justify-center items-center "> 
           <h1 className="text-5xl text-white font-sans text-center pt-24 pr-2">
             Welcome to BSE25-GRP-28
